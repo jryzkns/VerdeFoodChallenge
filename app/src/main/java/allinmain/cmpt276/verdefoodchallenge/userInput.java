8b48b2942b89cbb7e14e65eae604b581e9c79e4f
Jack Zhou <xza194@sfu.ca>
2018-10-12 21:31:22 -0700

    added two core classes: -userinput -CO2e classes contain empty functions and we are ready for some implementations.

---
+++ app/src/main/java/allinmain/cmpt276/verdefoodchallenge/userInput.java
@@ -0,0 +1,29 @@
+package allinmain.cmpt276.verdefoodchallenge;
+
+import java.util.HashMap;
+
+public class userInput {
+
+  private HashMap<String,Float> userDietTable;
+
+  userInput(){
+    //what would go here? hmmmmm
+  }
+
+  public void getInputs(){
+    /*
+    TODO: write a function that would fill the table
+    also for consideration, what if the user needs to update info
+    after the initial input? Consider writing a new function for it!
+    */
+  }
+
+  public void updateInputs(){
+    //see getInputs();
+  }
+
+  public HashMap<String,Float> export(){
+    return userDietTable;
+  }
+
+}
