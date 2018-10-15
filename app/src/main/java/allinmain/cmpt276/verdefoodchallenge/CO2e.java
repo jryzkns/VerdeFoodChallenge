f297bd694ff2421e782797fc4bd0eebad43a64e6
Jack Zhou <xza194@sfu.ca>
2018-10-14 19:16:00 -0700

    naive algorithm done, unit tests written

--- app/src/main/java/allinmain/cmpt276/verdefoodchallenge/CO2e.java
+++ app/src/main/java/allinmain/cmpt276/verdefoodchallenge/CO2e.java
@@ -1,29 +1,50 @@
 package allinmain.cmpt276.verdefoodchallenge;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class CO2e {
 
   private HashMap<String,Double> foodTable;
   private HashMap<String,Double> userCurrentDiet;
 
-  //set to NaN so we have a sentinel value for catching errors/exceptions
-  private Double userCO2e = Double.NaN;
+  private Double userCO2e;
 
 
-  CO2e(/*maybe take in a hashmap here, i don't know*/){
-    // TODO: read datafile and/or fill the foodTable with official data
-    // TODO: take in userinput.export(), write into userCurrentDiet
+  CO2e(HashMap<String,Double> userData, HashMap<String,Double> FoodData){
+
+    foodTable = new HashMap<>(FoodData);
+    userCurrentDiet = new HashMap<>(userData);
+
+    //set to NaN so we have a sentinel value for catching errors/exceptions
+    userCO2e = Double.NaN;
   }
 
-  private void calculate(){
-    // TODO: improvise Algorithm
-    // current idea: cycle through userCurrentDiet for entries and dot with
-    // matching entries in foodTable
+  public void calculate(){
+
+    if (userCurrentDiet.size() == 0){
+      return;
+    } else {
+      userCO2e = 0d;
+    }
+
+    for (Map.Entry<String,Double> food_item : userCurrentDiet.entrySet()){
+
+      String item_name = food_item.getKey();
+      Double consume_value = food_item.getValue();
+
+      if (foodTable.containsKey(item_name)){
+        userCO2e += consume_value*foodTable.get(item_name);
+      }
+
+    }
+
   }
 
   //default getter
   public Double getUserCO2e(){
+
     return userCO2e;
+
   }
 }
