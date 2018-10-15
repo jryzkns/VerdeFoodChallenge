98796f84778b47169bcf2b2b34100a2ed7cc1a46
Jack Zhou <xza194@sfu.ca>
2018-10-14 19:54:42 -0700

    cleaned up code ready to merge branch

--- app/src/main/java/allinmain/cmpt276/verdefoodchallenge/CO2e.java
+++ app/src/main/java/allinmain/cmpt276/verdefoodchallenge/CO2e.java
@@ -10,22 +10,26 @@ public class CO2e {
 
   private Double userCO2e;
 
-
   CO2e(HashMap<String,Double> userData, HashMap<String,Double> FoodData){
 
-    foodTable = new HashMap<>(FoodData);
     userCurrentDiet = new HashMap<>(userData);
+    foodTable = new HashMap<>(FoodData);
 
     //set to NaN so we have a sentinel value for catching errors/exceptions
     userCO2e = Double.NaN;
+
   }
 
   public void calculate(){
 
     if (userCurrentDiet.size() == 0){
+
       return;
+
     } else {
+
       userCO2e = 0d;
+
     }
 
     for (Map.Entry<String,Double> food_item : userCurrentDiet.entrySet()){
@@ -41,10 +45,25 @@ public class CO2e {
 
   }
 
-  //default getter
   public Double getUserCO2e(){
-
     return userCO2e;
+  }
+
+  public HashMap<String,Double> suggestDietChange(String policy){
+    //TODO: suggestion algorithm for minimizing CO2e output;
+    //outputs a hashmap of ***changes*** that should be made:
+    //-have the ability to introduce new foods
+    //-give negative values to foods that one should eat less
+    //-give positive values to foods that one should eat more
+    //-give 0 to foods that one should stay having the same amount
+
+    HashMap<String,Double> suggestion = new HashMap<>();
+
+    switch (policy){
+      case "": // different suggestions depending on the policy
+    }
+
+    return suggestion;
 
   }
 }
