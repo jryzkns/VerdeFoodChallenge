36f4c719c9cd6afeab26e7271651f5c4fbb6408d
Jack Zhou <xza194@sfu.ca>
2018-10-20 03:15:01 -0700

    extended CO2e.java so it is easier to work with
    fixed relevant unit tests

--- app/src/main/java/allinmain/cmpt276/verdefoodchallenge/CO2e.java
+++ app/src/main/java/allinmain/cmpt276/verdefoodchallenge/CO2e.java
@@ -7,49 +7,78 @@ public class CO2e {
 
   private HashMap<String,Double> foodTable;
   private HashMap<String,Double> userCurrentDiet;
+  private HashMap<String,Double> suggestion;
 
   private Double userCO2e;
 
-  CO2e(HashMap<String,Double> userData, HashMap<String,Double> FoodData){
+  // number taken from:
+  // https://en.wikipedia.org/wiki/Metro_Vancouver_Regional_District#Demographics
+  private static final int metroVanPopln = 2463431;
 
+  CO2e(HashMap<String,Double> userData){
+
+    // values in userData SHOULD be in yearly units!!!!!!!!
     userCurrentDiet = new HashMap<>(userData);
-    foodTable = new HashMap<>(FoodData);
+    suggestion = new HashMap<>();
+    foodTable = new HashMap<>();
+
+    //might do file i/o someday
+    foodTable.put("LAMB",39.2);
+    foodTable.put("BEEF",27.0);
+    foodTable.put("CHEESE",13.5);
+    foodTable.put("PORK",12.1);
+    foodTable.put("FARMED SALMON",11.9);
+    foodTable.put("TURKEY",10.9);
+    foodTable.put("CHICKEN",6.9);
+    foodTable.put("CANNED TUNA",6.1);
+    foodTable.put("EGGS",4.8);
+    foodTable.put("POTATOES",2.9);
+    foodTable.put("RICE",2.7);
+    foodTable.put("PEANUT BUTTER",2.5);
+    foodTable.put("NUTS",2.3);
+    foodTable.put("YOGURT",2.2);
+    foodTable.put("BROCCOLI",2.0);
+    foodTable.put("TOFU",2.0);
+    foodTable.put("DRY BEANS",2.0);
+    foodTable.put("MILK 2%",1.9);
+    foodTable.put("TOMATO",1.1);
+    foodTable.put("LENTILS",0.9);
 
     //set to NaN so we have a sentinel value for catching errors/exceptions
     userCO2e = Double.NaN;
 
   }
 
-  public void calculate(){
+  //As Per 3.1.4a; used both for user data and suggestion data
+  public Double calculate(HashMap<String,Double> in_table){
 
-    if (userCurrentDiet.size() == 0){
+    Double consumerCO2e = 0d;
 
-      return;
+    if (in_table.size() == 0){
+
+      consumerCO2e = Double.NaN;
 
     } else {
 
-      userCO2e = 0d;
+      for (Map.Entry<String,Double> food_item : in_table.entrySet()){
 
-    }
+        String item_name = food_item.getKey();
+        Double consume_value = food_item.getValue();
 
-    for (Map.Entry<String,Double> food_item : userCurrentDiet.entrySet()){
+        if (foodTable.containsKey(item_name)){
+          consumerCO2e += consume_value*foodTable.get(item_name);
+        }
 
-      String item_name = food_item.getKey();
-      Double consume_value = food_item.getValue();
-
-      if (foodTable.containsKey(item_name)){
-        userCO2e += consume_value*foodTable.get(item_name);
       }
 
     }
 
+    return consumerCO2e;
+
   }
 
-  public Double getUserCO2e(){
-    return userCO2e;
-  }
-
-  public HashMap<String,Double> suggestDietChange(String policy){
+  //As per requirement 3.1.5b
+  public HashMap<String,Double> makeDietChangeTable(String policy){
     //TODO: suggestion algorithm for minimizing CO2e output;
     //outputs a hashmap of ***changes*** that should be made:
     //-have the ability to introduce new foods
@@ -59,13 +88,48 @@ public class CO2e {
 
     HashMap<String,Double> suggestion = new HashMap<>();
 
-    //does adding this new branch work?
-
-    switch (policy){
-      case "": // different suggestions depending on the policy
+    switch (policy.toUpperCase()){
+      case "CARNIVORE":
+      case "LESSMEAT":
+      case "VEGETARIAN":
+      case "VEGAN":
+      //other ideas
     }
 
     return suggestion;
 
   }
+
+  //As per requirement 3.1.5a
+  public Double calculateDietSavings(){ return calculate(suggestion); }
+
+  //As per requirement 3.1.6a
+  public Double calculateMetroVanSavings(){ return metroVanPopln*calculateDietSavings();}
+
+  //DEFAULT GETTERS AND SETTERS
+
+  public Double getUserCO2e(){
+    return userCO2e;
+  }
+  public void setUserCO2e(Double userCO2e) {
+    this.userCO2e = userCO2e;
+  }
+
+
+  public HashMap<String, Double> getUserCurrentDiet() {
+    return userCurrentDiet;
+  }
+  public void setUserCurrentDiet(HashMap<String, Double> userCurrentDiet) {
+    // is this a shallow copy? do we only need a shallow copy?
+    this.userCurrentDiet = userCurrentDiet;
+  }
+
+  public HashMap<String, Double> getSuggestion() {
+    return suggestion;
+  }
+  public void setSuggestion(HashMap<String, Double> suggestion) {
+    this.suggestion = suggestion;
+  }
+
+
 }
--- app/src/test/java/allinmain/cmpt276/verdefoodchallenge/CO2eTest.java
+++ app/src/test/java/allinmain/cmpt276/verdefoodchallenge/CO2eTest.java
@@ -9,17 +9,16 @@ public class CO2eTest {
 
   @Test
   public void userCO2estartsoutasNaN(){
-    CO2e bareobject = new CO2e(new HashMap<String, Double>(),new HashMap<String, Double>());
+    CO2e bareobject = new CO2e(new HashMap<String, Double>());
     assertEquals(Double.NaN,bareobject.getUserCO2e(),0);
   }
 
   @Test
   public void calculationAlgorithmWorksOnEmptyData(){
 
-    CO2e emptytest = new CO2e(new HashMap<String, Double>(),new HashMap<String, Double>());
+    CO2e emptytest = new CO2e(new HashMap<String, Double>());
 
-    emptytest.calculate();
-    double result = emptytest.getUserCO2e();
+    double result = emptytest.calculate(emptytest.getUserCurrentDiet());
 
     assertEquals(Double.NaN, result,0);
 
@@ -28,24 +27,15 @@ public class CO2eTest {
   @Test
   public void calculatedAlgorithmWorksNormally(){
 
-    HashMap<String,Double> ref_values = new HashMap<>();
     HashMap<String,Double> user_values = new HashMap<>();
 
-    ref_values.put("test1",1d);
-    ref_values.put("test2",1d);
-    ref_values.put("test3",1d);
-    ref_values.put("test4",1d);
-    ref_values.put("test5",1d);
+    user_values.put("LENTILS",1d);
 
-    user_values.put("test1",1d);
-    user_values.put("test3",1d);
-    user_values.put("test6",1d);
+    CO2e run = new CO2e(user_values);
 
-    CO2e run = new CO2e(ref_values,user_values);
+    run.setUserCO2e(run.calculate(user_values));
 
-    run.calculate();
-
-    assertEquals(2d,run.getUserCO2e(),0);
+    assertEquals(0.9,run.getUserCO2e(),0);
   }
 
 }
