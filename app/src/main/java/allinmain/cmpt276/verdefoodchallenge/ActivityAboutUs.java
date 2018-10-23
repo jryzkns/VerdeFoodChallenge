a98684d5770556b3a6eeb08e868fdf57a6bd75a9
jryzkns <xza194@sfu.ca>
2018-10-22 20:56:27 -0700

    new files

---
+++ app/src/main/java/allinmain/cmpt276/verdefoodchallenge/ActivityAboutUs.java
@@ -0,0 +1,28 @@
+package allinmain.cmpt276.verdefoodchallenge;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridView;
+
+public class ActivityAboutUs extends Activity implements View.OnClickListener {
+    private Button iknow;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aboutus);
+        init();
+    }
+    private void init()
+    {
+        iknow=(Button)this.findViewById(R.id.iknow);
+        iknow.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        this.finish();
+    }
+}
---
+++ app/src/main/java/allinmain/cmpt276/verdefoodchallenge/AdviceActivity.java
@@ -0,0 +1,36 @@
+package allinmain.cmpt276.verdefoodchallenge;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class AdviceActivity extends Activity implements View.OnClickListener{
+    private Button back,recal;
+    private TextView calinfo;
+    private DataCenter dc=DataCenter.getInstance();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_advice);
+        init();
+    }
+    private void init()
+    {
+        back=(Button)this.findViewById(R.id.back);
+        recal=(Button)this.findViewById(R.id.recal);
+        calinfo=(TextView)this.findViewById(R.id.calinfo);
+        back.setOnClickListener(this);
+        recal.setOnClickListener(this);
+        calinfo.setText(dc.getDietInfo());
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        this.finish();
+    }
+}
