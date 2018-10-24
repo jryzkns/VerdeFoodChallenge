a2274a002752f0a0b318b8f73a13bc629b33efd5
jryzkns <xza194@sfu.ca>
2018-10-23 17:45:15 -0700

    Injected code from my previous CO2e.java; suggestion algorithm currently empty and needs adaptation

--- app/src/main/java/allinmain/cmpt276/verdefoodchallenge/DataCenter.java
+++ app/src/main/java/allinmain/cmpt276/verdefoodchallenge/DataCenter.java
@@ -1,12 +1,31 @@
 package allinmain.cmpt276.verdefoodchallenge;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Random;
+
 public class DataCenter {
     private static DataCenter mDataCenter=null;
     private ArrayList<Food> mFoodLst;
     private HashMap<Integer,Float> dietInfo;
+
+    private HashMap<Integer,Float> suggestion;
+
+    // number taken from:
+    // https://en.wikipedia.org/wiki/Metro_Vancouver_Regional_District#Demographics
+    private static final int metroVanPopln = 2463431;
+
+    private Random picker; // for helping with picking food choices randomly
+
+    private HashSet meats;
+    private HashSet altProtein;
+    private HashSet vegetables;
+
     private DataCenter()
     {
 
@@ -23,7 +42,8 @@ public class DataCenter {
     private void init()
     {
         mFoodLst=new ArrayList<Food>();
-        dietInfo=new  HashMap<Integer, Float> ();
+        dietInfo=new  HashMap<>();
+        suggestion=new HashMap<>();
 
         mFoodLst.add(new Food("Beef",R.mipmap.beef,27f,""));
         mFoodLst.add(new Food("Lentils",R.mipmap.biandou,0.9f,""));
@@ -45,6 +65,13 @@ public class DataCenter {
         mFoodLst.add(new Food("Yogurt",R.mipmap.suannai,2.2f,""));
         mFoodLst.add(new Food("Tomato",R.mipmap.tomato,1.1f,""));
         mFoodLst.add(new Food("Broccoli",R.mipmap.xilanhua,2f,""));
+
+        picker = new Random();
+        meats = new HashSet<>(Arrays.asList(  "LAMB", "BEEF", "PORK", "FARMED SALMON",
+                "TURKEY", "CHICKEN", "CANNED TUNA"));
+        altProtein = new HashSet<>(Arrays.asList( "NUTS","DRIED BEANS","LENTILS",
+                "TOFU","PEANUT BUTTER"));
+        vegetables = new HashSet<>(Arrays.asList("BROCCOLI","TOMATO"));
 
     }
 
@@ -72,7 +99,12 @@ public class DataCenter {
     public void resetDiet()
     {
         dietInfo.clear();
+        suggestion.clear();
     }
+
+    //try to merge these two functions so it's more abstractable;
+    //we running short on time here
+
     public float calDietCo2()
     {
         float result=0;
@@ -83,17 +115,196 @@ public class DataCenter {
 
         return result;
     }
+
+    //As per requirement 3.1.5a
+    public float calSuggestCo2()
+    {
+        float result=0;
+        for(Entry<Integer,Float> item:suggestion.entrySet())
+        {
+            result+=mFoodLst.get(item.getKey()).getCo2()*item.getValue();
+        }
+
+        return result;
+    }
     public String getDietInfo()
     {
         String result="";
 
+//        potentially getting rid of this in the end product
         for(Entry<Integer,Float> item:dietInfo.entrySet())
         {
-
-            result+=mFoodLst.get(item.getKey()).getName()+":"+item.getValue()+" kg/week\n";
+            result+=mFoodLst.get(item.getKey()).getName()+":"+item.getValue()+" kg/wk\n";
         }
-        result+="\nYou will cost :"+calDietCo2()*52+"kg Co2 in one year";
+
+        result+="With your current diet, you will generate around ";
+        result+=(int)calDietCo2()*52;
+        result+="kg of CO2e in a year";
+
         return result;
     }
+
+
+    public String getSuggestionInfo(){
+
+        String result="";
+
+//        potentially getting rid of this in the end product
+        for(Entry<Integer,Float> item:suggestion.entrySet())
+        {
+            result+=mFoodLst.get(item.getKey()).getName()+":"+item.getValue()+" kg/wk\n";
+        }
+
+        result+="With a change in diet, you could be saving ";
+        result+=(int)calSuggestCo2()*52;
+        result+="kg of CO2e in a year\n\n";
+        result+="The Metro Vancouver area can collectively save ";
+        result+=(int)calculateMetroVanSavings();
+        result+="kg of CO2e in a year if everyone took this change in diet!";
+        return result;
+    }
+
+    //As per requirement 3.1.5b
+    public HashMap<Integer, Float> makeDietChangeTable(String policy){
+
+        HashMap<Integer,Float> suggestion = new HashMap<>();
+
+        switch (policy.toUpperCase()){
+            case "CARNIVORE":
+
+                // general idea is to just eat less of all the meats and have more chicken instead
+                // KFC for life
+//
+                for(Map.Entry<Integer,Float> food_item : dietInfo.entrySet()){
+//                    String item_name = food_item.getKey();
+//                    Float currentConsumeValue = food_item.getValue();
+//
+//                    if ( item_name != "CHICKEN" && meats.contains(item_name) ){
+//
+//                        if (!suggestion.containsKey(item_name)){
+//                            suggestion.put(item_name,-currentConsumeValue*0.3);
+//                        }
+//
+//                        if (!suggestion.containsKey("CHICKEN")) {
+//                            suggestion.put("CHICKEN",currentConsumeValue*0.3);
+//                        } else {
+//                            suggestion.put("CHICKEN",suggestion.get("CHICKEN")+currentConsumeValue*0.3);
+//                        }
+//
+//                    }
+//
+                }
+
+            case "LESSMEAT":
+
+                //if it's meat, let's switch some of it to beans and tofu
+//
+//                for(Map.Entry<Integer,Float> food_item : dietInfo.entrySet()){
+//                    String item_name = food_item.getKey();
+//                    Double currentConsumeValue = food_item.getValue();
+//
+//                    if (meats.contains(item_name)){
+//
+//                        Iterator<String> altProts= altProtein.iterator();
+//                        for (int i = 0; i < picker.nextInt(altProtein.size()) - 1; i++){
+//                            altProts.next();
+//                        }
+//
+//                        String chosenAltProt = altProts.next();
+//
+//                        if (!suggestion.containsKey(item_name)) {
+//                            suggestion.put(item_name, -currentConsumeValue * 0.3);
+//                        }
+//
+//                        if (!suggestion.containsKey(chosenAltProt)) {
+//                            suggestion.put(chosenAltProt,currentConsumeValue*0.3);
+//                        } else {
+//                            suggestion.put(chosenAltProt,suggestion.get(chosenAltProt)+currentConsumeValue*0.3);
+//                        }
+//
+//                    }
+//
+//                }
+
+            case "VEGETARIAN":
+//
+//                // LESSMEAT policy but more severe and encourages eggs
+//
+//                for(Map.Entry<Integer,Float> food_item : dietInfo.entrySet()){
+//                    String item_name = food_item.getKey();
+//                    Double currentConsumeValue = food_item.getValue();
+//
+//                    if (meats.contains(item_name)){
+//
+//                        HashSet<String> altsWithEggs = altProtein;
+//                        altsWithEggs.add("EGGS");
+//                        Iterator<String> altProts= altsWithEggs.iterator();
+//                        for (int i = 0; i < picker.nextInt(altProtein.size()+1) - 1; i++){
+//                            altProts.next();
+//                        }
+//
+//                        String chosenAltProt = altProts.next();
+//
+//                        if (!suggestion.containsKey(item_name)) {
+//                            suggestion.put(item_name, -currentConsumeValue);
+//                        }
+//
+//                        if (!suggestion.containsKey(chosenAltProt)) {
+//                            suggestion.put(chosenAltProt,currentConsumeValue*0.7);
+//                        } else {
+//                            suggestion.put(chosenAltProt,suggestion.get(chosenAltProt)+currentConsumeValue*0.3);
+//                        }
+//
+//                    }
+//
+//                }
+
+            case "VEGAN":
+
+//                // policy "VEGETARIAN" but with no eggs
+//
+//                for(Map.Entry<Integer,Float> food_item : dietInfo.entrySet()){
+//                    String item_name = food_item.getKey();
+//                    Double currentConsumeValue = food_item.getValue();
+//
+//                    if (meats.contains(item_name)){
+//
+//                        Iterator<String> altProts= altProtein.iterator();
+//                        for (int i = 0; i < picker.nextInt(altProtein.size()) - 1; i++){
+//                            altProts.next();
+//                        }
+//
+//                        String chosenAltProt = altProts.next();
+//
+//                        if (!suggestion.containsKey(item_name)) {
+//                            suggestion.put(item_name, -currentConsumeValue);
+//                        }
+//
+//                        if (!suggestion.containsKey(chosenAltProt)) {
+//                            suggestion.put(chosenAltProt,currentConsumeValue);
+//                        } else {
+//                            suggestion.put(chosenAltProt,suggestion.get(chosenAltProt)+currentConsumeValue*0.3);
+//                        }
+//
+//                    } else if (item_name == "EGGS"){
+//
+//                        if (!suggestion.containsKey("EGGS")) {
+//                            suggestion.put("EEGS", -currentConsumeValue);
+//                        }
+//
+//                    }
+//
+//                }
+
+                //other ideas?
+        }
+
+        return suggestion;
+
+    }
+
+    //As per requirement 3.1.6a
+    public float calculateMetroVanSavings(){ return metroVanPopln*calSuggestCo2();}
+
 
 }
