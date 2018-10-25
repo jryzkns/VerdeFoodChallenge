8663907208c0e25723fd9cbf9898cb2f38e675a8
MinJun Lee <mla222@sfu.ca>
2018-10-25 16:58:36 -0700

    I commend out the error import

--- app/src/main/java/allinmain/cmpt276/verdefoodchallenge/Co2CalActivity.java
+++ app/src/main/java/allinmain/cmpt276/verdefoodchallenge/Co2CalActivity.java
@@ -3,7 +3,7 @@ package allinmain.cmpt276.verdefoodchallenge;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+//import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
