723eee382c65f5c42b8134ca09fb4dd6523e336c
heweic <hewei_cao@sfu.ca>
2018-10-30 18:02:34 -0700

    Upload manage_profile activity

---
+++ app/src/main/java/allinmain/cmpt276/verdefoodchallenge/Manage_Profile.java
@@ -0,0 +1,21 @@
+package allinmain.cmpt276.verdefoodchallenge;
+
+import android.app.Activity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class Manage_Profile extends Activity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manage__profile);
+    }
+    private void inti(){}
+
+    public void onClick(View view){
+
+
+    }
+}
