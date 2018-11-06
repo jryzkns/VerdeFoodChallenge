fb43930919e6fdfc7256b31415f8209cd953a814
jryzkns <xza194@sfu.ca>
2018-11-05 23:54:03 -0800

    Basic reading from DB done

---
+++ app/src/main/java/allinmain/cmpt276/verdefoodchallenge/Pledgelist.java
@@ -0,0 +1,45 @@
+package allinmain.cmpt276.verdefoodchallenge;
+
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class Pledgelist extends ArrayAdapter<UserInformation> {
+
+    private Activity context;
+    private List<UserInformation> pledgeList;
+
+    public Pledgelist(Activity context, List<UserInformation> pledgeList){
+        super(context, R.layout.list_layout,pledgeList);
+        this.context = context;
+        this.pledgeList = pledgeList;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        LayoutInflater inflater = context.getLayoutInflater();
+
+        View listViewItem = inflater.inflate(R.layout.list_layout, null, true);
+
+        TextView tvName = listViewItem.findViewById(R.id.listusername);
+        TextView tvLocation = listViewItem.findViewById(R.id.listuserlocation);
+        TextView tvPledge = listViewItem.findViewById(R.id.listuserPledge);
+
+        UserInformation user = pledgeList.get(position);
+
+
+        tvName.setText(user.getNAME());
+        tvLocation.setText(user.getLOCATION());
+        tvPledge.setText(user.getPLEDGEAMOUNT().toString());
+
+        return listViewItem;
+    }
+}
--- app/src/main/java/allinmain/cmpt276/verdefoodchallenge/community.java
+++ app/src/main/java/allinmain/cmpt276/verdefoodchallenge/community.java
@@ -2,14 +2,30 @@ package allinmain.cmpt276.verdefoodchallenge;
 
 import android.app.Activity;
 import android.content.Intent;
-//import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.firebase.auth.UserInfo;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class community extends Activity implements View.OnClickListener {
+
+    private ListView listViewPledges;
+
+    DatabaseReference DBPledges;
+
+    List<UserInformation> pledgeList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +35,7 @@ public class community extends Activity implements View.OnClickListener {
     }
 
     private void init(){
-        ImageView about = findViewById(R.id.about_profile);
+//        ImageView about = findViewById(R.id.about_profile);
         TextView map_all=findViewById(R.id.all_map_community);
         TextView map_V=findViewById(R.id.V_map_community);
         TextView map_B=findViewById(R.id.B_map_community);
@@ -28,7 +44,7 @@ public class community extends Activity implements View.OnClickListener {
         TextView map_R=findViewById(R.id.R_map_community);
         TextView map_NW=findViewById(R.id.NW_map_community);
 
-        about.setOnClickListener(this);
+//        about.setOnClickListener(this);
         map_all.setOnClickListener(this);
         map_V.setOnClickListener(this);
         map_B.setOnClickListener(this);
@@ -37,8 +53,40 @@ public class community extends Activity implements View.OnClickListener {
         map_R.setOnClickListener(this);
         map_NW.setOnClickListener(this);
 
+        listViewPledges = findViewById(R.id.listviewPledges);
+
+        DBPledges = FirebaseDatabase.getInstance().getReference("USERINFO");
+
+        pledgeList = new ArrayList<>();
+
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        DBPledges.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                pledgeList.clear();
+                for(DataSnapshot pledgeSnapshot: dataSnapshot.getChildren()){
+                    UserInformation user = pledgeSnapshot.getValue(UserInformation.class);
+
+                    pledgeList.add(user);
+                }
+
+                Pledgelist adapter = new Pledgelist(community.this, pledgeList);
+                listViewPledges.setAdapter(adapter);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
 
     @Override
     public void onClick(View view) {
@@ -65,11 +113,16 @@ public class community extends Activity implements View.OnClickListener {
             case R.id.NW_map_community:
                 map.setBackgroundResource(R.drawable.new_westminster);
                 break;
-            case R.id.about_community:
-                Intent about =new Intent(community.this,ActivityAboutUs.class);
-                community.this.startActivity(about);
-                break;
+//            case R.id.about_community:
+//                Intent about =new Intent(community.this,ActivityAboutUs.class);
+//                community.this.startActivity(about);
+//                break;
         }
 
     }
+
+    //TODO watch video on how to pull data from DB
+    //TODO write the calculation algorithm
+
+
 }
