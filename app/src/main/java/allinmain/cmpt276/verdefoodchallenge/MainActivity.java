072d7f22954d127d0ca1a51e2cb0b3515e89f24f
xza194 <xza194@sfu.ca>
2018-11-21 11:24:18 -0800

    Revert "Merge remote-tracking branch 'remotes/origin/master' into 37-ui-import-and-pie-chart-add-in"
    This reverts commit abff60a56077b944a2bdb305a54df5cb4e09bd38

--- app/src/main/java/allinmain/cmpt276/verdefoodchallenge/MainActivity.java
+++ app/src/main/java/allinmain/cmpt276/verdefoodchallenge/MainActivity.java
@@ -1,7 +1,10 @@
 package allinmain.cmpt276.verdefoodchallenge;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+//import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.support.annotation.NonNull;
@@ -23,7 +26,6 @@ import com.felipecsl.asymmetricgridview.library.widget.AsymmetricGridView;
 import com.felipecsl.asymmetricgridview.library.widget.AsymmetricGridViewAdapter;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class MainActivity extends Activity{
 
 import java.util.ArrayList;
 import java.util.List;
@@ -149,11 +151,23 @@ public class MainActivity extends Activity implements CompoundButton.OnCheckedCh
 
     private void init()
     {
-        start = findViewById(R.id.start);
+        sp = getSharedPreferences("User", Context.MODE_PRIVATE);
+        if(sp.getInt("gender",-1)!=-1)
+        {
+           // this.startActivity(new Intent(this,GreenFoodActivity.class));
+            //this.finish();
+        };
+        //malerb=(RadioButton)this.findViewById(R.id.malerb);
+        //femalerb=(RadioButton)this.findViewById(R.id.femalerb);
+        start=(Button)this.findViewById(R.id.start);
+        //malerb.setOnCheckedChangeListener(this);
+        //femalerb.setOnCheckedChangeListener(this);
+
         start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 MainActivity.this.startActivity(new Intent(MainActivity.this,login.class));
+                MainActivity.this.finish();
             }
         });
     }
@@ -212,4 +226,28 @@ public class MainActivity extends Activity implements CompoundButton.OnCheckedCh
     }
 
 
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+
+        SharedPreferences.Editor edit = sp.edit();
+        /*switch (compoundButton.getId())
+        {
+            case R.id.malerb:
+                if(b)
+                {
+                    edit.putInt("gender",1);
+                    edit.commit();
+                }
+                break;
+            case R.id.femalerb:
+                edit.putInt("gender",0);
+                edit.commit();
+                break;
+        }*/
+    }
+
+    @Override
+    public void onPointerCaptureChanged( boolean hasCapture ) {
+
+    }
 }
